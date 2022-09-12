90368b48ead4d69d7d1ed822e6096681e60945a4
ManojTarakaRamaraoGunuru <manoj.gtr@iitgn.ac.in>
2022-09-12 06:26:06 -0400

    crypto file (helper functions) added

--- Assignment1/crypto.py
+++ Assignment1/crypto.py
@@ -26,16 +26,6 @@ def encrypt(string,opt,val=2):
     if opt == 1:
         return string
     elif opt == 2:
-        # lt = string.split(" ")
-        # ans = ""
-        # for word in lt:
-        #     for char in word:
-        #         if char.isupper():
-        #             ans += chr((ord(char)+val -65)%26 + 65)
-        #         elif char.islower():
-        #             ans += chr((ord(char)+val -97)%26 + 97)
-        #     ans+=" "
-        # return ans[:-1]
         ans = ""
         l = 0
         r = 0
@@ -45,7 +35,6 @@ def encrypt(string,opt,val=2):
         temp = ""
         while(r<len(string)):
             if(string[r] == "\n" or string[r] == "\r" or string[r] == " "):
-                # a = string[l:r]
                 ans += temp
                 temp = ""
                 while(r<len(string) and (string[r] == "\n" or string[r] == "\r" or string[r] == " ")):
@@ -57,7 +46,6 @@ def encrypt(string,opt,val=2):
                     temp += chr((ord(string[r])+val -65)%26 + 65)
                 elif string[r].islower():
                     temp += chr((ord(string[r])+val -97)%26 + 97)
-                # temp+=string[r]
                 r += 1
         ans+=temp
         return ans
@@ -69,7 +57,7 @@ def encrypt(string,opt,val=2):
 def decrypt(string,opt,val = 24):
     return encrypt(string, opt, val)
 
-
+# Check the helper functions by uncommenting this
 # a = "   Server filestatus ok  \n\n p"
 # print(a)
 # a = encrypt(a,2)
