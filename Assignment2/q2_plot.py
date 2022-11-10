dafd702e3c0fbdaadbb300532862ae353e32aad7
ManojTarakaRamaraoGunuru <manoj.gtr@iitgn.ac.in>
2022-11-10 15:23:07 -0500

    2(e) correction done

--- Assignment2/q2_plot.py
+++ Assignment2/q2_plot.py
@@ -10,27 +10,22 @@ def get_vals(stats):
 
 T = 120 # total time
 intvl = 1 # interval
-file1 = 'g_B_1'
-file2 = 'g_C_1'
-file3 = 'g_D_1'
-question = 'g'
-iteration = '1'
+file1 = 'e_B_3'
+file2 = 'e_C_3'
+iteration = '3'
 with open(file1 + '.txt') as f:
     stats1 = f.read()
 with open(file2 + '.txt') as f:
     stats2 = f.read()
-with open(file3 + '.txt') as f:
-    stats3 = f.read()
+question = 'e'
 
 tp1 = get_vals(stats1)
 tp2 = get_vals(stats2)
-tp3 = get_vals(stats3)
 t = np.arange(0, T, intvl)
 plt.plot(t, tp1)
 plt.plot(t, tp2)
-plt.plot(t, tp3)
 plt.xlabel('time (sec)')
 plt.ylabel('throughput (Mbps)')
-plt.ylim(0, 600)
-plt.legend(["throughput at B", "throughput at C", "throughput at D"])
+plt.ylim(0, 300)
+plt.legend(["throughput at B", "throughput at C"])
 plt.savefig('2' + question + iteration + '.png')
